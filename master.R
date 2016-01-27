f8ec7656fb14ecedd4d069dda110ab10a3b69c95
jgoldfinch <jessie.d.golding@gmail.com>
2016-01-26 17:06:09 -0700

    Update
    Removed text related to JG project

--- master.R
+++ master.R
@@ -6,42 +6,24 @@
 ###Master code###
 
 #Set working directory
-setwd("C:\\Users\\jessie.golding\\Documents\\GitHub\\MDAM")
+#CHANGE when changing users
+setwd("C:/Users/jessie.golding/Documents/GitHub/MDAM")
 
 #Load packages needed for MDAM
 source("run_loadpackages.R")
 
 #Load pilot season data 
 
-#Read data from csv file using access to R file
+#Read data from csv file using format file nbr.mdam.R (which cuts down the number of species to 4)
 source("nbr.mdam.R")
 
-#Output is dataframe mdam with all data from 2015 Pilot Season
+#Output is dataframe "mdam" with all data from 2015 Pilot Season
 
-#Data from NBR is already formatted.  Update format file later.
+source("run_MDAM.R")
 
-source("run_MDAMextension_cor.R")
+save(out,file="pilot")
 
-save(out,file="Ch1")
-
-###Plots and Tables - Chapter 1###
-load("Ch1")
-source("plot_MDAMextension_cor.R")
+###Code for loading the saved output later###
+load("pilot")
 
 
-###MDAM - Chapter 2###
-source("run_MDAMCh2.R")
-save(out,file="Ch2")
-load("Ch2")
-
-###MDAM  - Chapter 2###
-source("plot_MDAMextension_corCh2.R")
-
-
-###MDAM - ALL SP###
-source("run_MDAMextension_ALL.R")
-save(out,file="ALL")
-load("ALL")
-
-### MDAM  - Plots for ALL SP ###
-source("plot_MDAMextension_corCh2.R")
