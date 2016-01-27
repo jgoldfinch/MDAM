d65de7a72cace13d2b6ca375dec5cdad86e0921b
jgoldfinch <jessie.d.golding@gmail.com>
2016-01-26 17:09:36 -0700

    Remove subset command
    Remove code to subset to just 4 species

--- nbr.mdam.R
+++ nbr.mdam.R
@@ -1,6 +1,5 @@
 mdam<-read.csv("mdamdata.csv")
-mdam<-subset(mdam, mdam$sp==10|mdam$sp==19|mdam$sp==32|mdam$sp==34)
+#mdam<-subset(mdam, mdam$sp==10|mdam$sp==19|mdam$sp==32|mdam$sp==34)
 
-mdam$sp<-ifelse(mdam$sp==10, 1, ifelse(mdam$sp==19,2, ifelse(mdam$sp==32,3, ifelse(mdam$sp==34, 4, 0))))
-
-
+#mdam$sp<-ifelse(mdam$sp==10, 1, ifelse(mdam$sp==19,2, ifelse(mdam$sp==32,3, ifelse(mdam$sp==34, 4, 0))))
+mdam<-mdam[order(mdam[,2],mdam[,3],mdam[,4]),]
